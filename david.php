1f3ff9853fb3f3f58e338b575a3349903bef0e99
olasode-david <60571573+olasode-david@users.noreply.github.com>
2020-06-06 15:24:35 +0400

    Create david.php

---
+++ david.php
@@ -0,0 +1,7 @@
+  function greetings_hng($name, $email, $hnd_id, $language)
+   {
+      data = "Hello world, my name is $name  with HNGi7ID $hnd_id using language $language for stage 2 task email $email ";
+          return data;
+   }
+   
+   echo greetings_hng("olasode","PHP","HNG-03287","olasodedavid@gmail.com");
